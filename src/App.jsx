8a2b5e07e4a297fb0cf930d444f108949896459a
VadymStratiienko <101202902+VadymStratiienko@users.noreply.github.com>
2022-08-15 12:13:05 +0300

    async

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { addTodo } from './store/todoSlice';
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addTodo, fetchTodos } from './store/todoSlice';
 import './App.css';
 import TodoList from './components/TodoList';
 import InputField from './components/InputField';
@@ -9,12 +9,16 @@ import InputField from './components/InputField';
 function App() {
   const [text,setText] = useState('')
   const dispatch = useDispatch();
+  const {status, error} = useSelector(state => state.todos);
 
   const addTask = () => {
     dispatch(addTodo({text}))
     setText('') 
   };
 
+  useEffect (() => {
+    dispatch(fetchTodos());
+  },[dispatch])
 
   return (
     <div className="App">
@@ -23,6 +27,8 @@ function App() {
      handleInput={setText}
      handleSubmit={addTask}
     />
+    {status === 'loading' && <h2>Loading...</h2>}
+    {error && <h2>An error occerd: {error}</h2>}
    <TodoList />
     </div>
   );
--- src/store/todoSlice.js
+++ src/store/todoSlice.js
@@ -1,9 +1,31 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
- const todoSlice = createSlice({
+export const fetchTodos = createAsyncThunk(
+  "todos/fetchTodos",
+  async function (_,{rejectWithValue}) {
+    try {      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos_limit=10"
+      );
+      console.log(response);
+
+      if(!response.ok) {
+        throw new Error('Server Error');
+      }
+
+      const data = await response.json();
+
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+  }
+});
+
+const todoSlice = createSlice({
   name: "todos",
   initialState: {
     todos: [],
+    status: null,
+    errors: null,
   },
   reducers: {
     addTodo(state, action) {
@@ -14,15 +36,31 @@ import { createSlice } from "@reduxjs/toolkit";
       });
     },
     removeTodo(state, action) {
-        state.todos=state.todos.filter(todo => todo.id !== action.payload.id)
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload.id);
     },
     toggleTodoComplete(state, action) {
-        const toggleTodo = state.todos.find(todo => todo.id === action.payload.id);
-        toggleTodo.comleted = !toggleTodo.comleted;
+      const toggleTodo = state.todos.find(
+        (todo) => todo.id === action.payload.id
+      );
+      toggleTodo.comleted = !toggleTodo.comleted;
+    },
+  },
+  extraReducers: {
+    [fetchTodos.pending]: (state, action) => {
+      state.status = "loading";
+      state.error = null;
+    },
+    [fetchTodos.fulfilled]: (state, action) => {
+      state.status = "resolved";
+      state.todos = action.payload;
+    },
+    [fetchTodos.rejected]: (state, action) => {
+      state.status = "rejected";
+      state.error = action.payload;
     },
   },
 });
 
-export const {addTodo, removeTodo, toggleTodoComplete} = todoSlice.actions
+export const { addTodo, removeTodo, toggleTodoComplete } = todoSlice.actions;
 
-export default todoSlice.reducer
+export default todoSlice.reducer;
